7fd66adb52196a75f5eefcd467bca9b1f97b777d
fossfreedom <foss.freedom@gmail.com>
2017-02-18 21:06:25 +0000

    theme qt apps - #46

--- etc/profile.d/10-budgie-desktop.sh
+++ etc/profile.d/10-budgie-desktop.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export QT_STYLE_OVERRIDE=gtk
+
+# force environment variable removal to allow QT apps to be themed correctly
+# this is new due to zesty including QT 5.8
+unset QT_STYLE_OVERRIDE
