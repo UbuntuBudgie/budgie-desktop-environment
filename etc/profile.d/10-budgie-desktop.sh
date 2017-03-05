11a3bcc32c08800b84679a0d7300d474787d06de
David Mohammed <fossfreedom@ubuntu.com>
2017-03-05 23:49:11 +0000

    qt5 theming with gtk3 themes #46

--- etc/profile.d/10-budgie-desktop.sh
+++ etc/profile.d/10-budgie-desktop.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# force environment variable removal to allow QT apps to be themed correctly
+# force QT override to not have a value to allow QT apps to be themed correctly
 # this is new due to zesty including QT 5.8
-unset QT_STYLE_OVERRIDE
+export QT_STYLE_OVERRIDE=
+
+# with qt-style-plugins ensure QT apps now pickup a GTK theme
+export QT_QPA_PLATFORMTHEME=gtk2
