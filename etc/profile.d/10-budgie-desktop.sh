1aeb07f869e5c5ddd4cd02fb22baf4ed797af4b9
David Mohammed <fossfreedom@ubuntu.com>
2017-05-03 16:28:28 +0100

    just use profile to make changes for Tilix

--- etc/profile.d/10-budgie-desktop.sh
+++ etc/profile.d/10-budgie-desktop.sh
@@ -4,7 +4,8 @@
 # this is new due to zesty including QT 5.8
 export QT_STYLE_OVERRIDE=
 
-# with qt-style-plugins ensure QT apps now pickup a GTK theme
+# with qt-style-plugins ensure QT apps now pickup a GTK+2 theme
+# N.B. we don't have in Ubuntu a gtk3 based override yet.
 export QT_QPA_PLATFORMTHEME=gtk2
 
 # for the live CD ensure we override chromium-browser
@@ -17,3 +18,20 @@ then
    sudo cp /usr/share/budgie-desktop/chromium-browser.desktop /usr/share/applications
 fi
 
+# Tilix needs to include a bash statement to source vte otherwise an error dialog
+# is displayed and proper folder navigation when creating new tiled windows is
+# not available.  Since Ubuntu's bash.bashrc does NOT execute shell scripts as per
+# Fedora, we'll have to mimic this via the login shell execution of this script.
+
+# be safe - if TILIX_ID already exists in .bashrc lets assume
+# either we have already done this via another run or
+# another package has made similar changes in this area
+
+if ! grep -q "TILIX_ID" ~/.bashrc; then
+    # lets delete 17.04 Terminix that was added
+    sed -i.bak '/# Ubuntu Budgie END$/d' ~/.bashrc
+    rm -f ~/.bashrc.bak
+
+    # now append vte source
+    cat /usr/share/budgie-desktop/vteprompt.txt >> ~/.bashrc
+fi
