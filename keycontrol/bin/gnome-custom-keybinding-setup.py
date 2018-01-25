bcdadb9f514b95458d7b6b44b530a574250c8a03
Bernard Gray <bernard_gray@debortoli.com.au>
2018-01-25 12:42:14 +1100

    enable proper logging process root '/' section before other sections  to fix timing issue where keys exist but dont work add verbose output option add sensible default key dir option

--- keycontrol/bin/gnome-custom-keybinding-setup.py
+++ keycontrol/bin/gnome-custom-keybinding-setup.py
@@ -10,13 +10,17 @@ from subprocess import *
 import argparse 
 import ast
 import configparser
+import logging as log
 import os
+import sys
 import tempfile
 
-# possible suggestion for providing system key location, which we can override
-#BUDGIE_KEYS = '/usr/share/budgie/custom-keys.d/
+SYS_KEYS = '/usr/share/budgie-desktop/keycontrol/custom-keys.d/'
+# proposals only follow, they are not implemented
+#ETC_KEYS = '/etc/budgie-desktop/keycontrol/custom-keys.d/'
+#USR_KEYS = '${HOME}/.config/budgie-desktop/keycontrol/custom-keys.d/'
 
-KEY_LOC = '/org/gnome/settings-daemon/plugins/media-keys/'
+DCONF_KEY_PATH = '/org/gnome/settings-daemon/plugins/media-keys/'
 
 class BudgieKeyError(Exception):
     '''A generic error handler that does nothing.'''
@@ -49,28 +53,27 @@ def getKeyFiles(key_dir):
                 _full_path = os.path.abspath(os.path.join(dirpath, f))
                 _file_list.append(os.path.abspath(os.path.join(dirpath, f)))
             else:
-                print('DEBUG: skipping %s, doesnt end in .dconf' % f)
+                log.debug('skipping %s, doesnt end in .dconf' % f)
     return _file_list
 
 def loadDconf(dconf_file):
     '''write a key file into the dconf database'''
     if args.dry_run:
-        print ('INFO: --dry-run active - not loading into dconf database, generated config follows')
-        print ('==============================================================================')
+        log.debug('--dry-run active - not loading into dconf database, generated config follows')
+        log.debug('==============================================================================')
         try:
             cmd = Popen(['cat', dconf_file], close_fds=True)
         except:
-            print('ERROR: problem executing command: %s' % ' '.join(cmd))
+            log.error('problem executing command: %s' % ' '.join(cmd))
             raise BudgieKeyError
     else:
         try:
-            print("INFO: live run, writing from %s" % dconf_file)
             cmd = Popen(['cat', dconf_file], stdout=PIPE)
-            cmd_pipe = Popen(['dconf', 'load', KEY_LOC], stdin = cmd.stdout, stdout = PIPE)
+            cmd_pipe = Popen(['dconf', 'load', DCONF_KEY_PATH], stdin = cmd.stdout, stdout = PIPE)
             cmd.stdout.close()
             cout = cmd_pipe.communicate()[0]
         except:
-            print('ERROR: problem executing command: %s' % ' '.join(cmd_pipe))
+            log.error('problem executing command: %s' % ' '.join(cmd_pipe))
             raise BudgieKeyError
 
 if __name__ == '__main__':
@@ -83,16 +86,26 @@ if __name__ == '__main__':
             '-f', '--force',  action = 'store_true', 
             help = 'overwrite existing key values'
     )
-    group = aparse.add_mutually_exclusive_group(required=True)
+    aparse.add_argument(
+            '-v', '--verbose',  action = 'store_true', 
+            help = 'increased output and informational/error messages'
+    )
+    group = aparse.add_mutually_exclusive_group()
     group.add_argument(
             '-d', '--dconf-file', 
 	    help = 'single dconf file to process'
     )
     group.add_argument(
-            '-D', '--dconf-dir', 
+            '-D', '--dconf-dir', default=SYS_KEYS,
 	    help = 'directory of dconf custom-keybinding files to process'
     )
     args = aparse.parse_args()
+
+    # setup logging
+    if args.verbose:
+        log.basicConfig(level='DEBUG', format='%(levelname)s: %(message)s')
+    else:
+        log.basicConfig(level='INFO', format='%(levelname)s: %(message)s')
 
     # identify the files we want to parse
     if args.dconf_file:
@@ -101,7 +114,7 @@ if __name__ == '__main__':
         dconf_files = getKeyFiles(args.dconf_dir)
 
     if len(dconf_files) == 0:
-        print('WARNING: no files found to process')
+        log.warning('no files found to process in (' + str(args.dconf_dir) + ', ' + str(args.dconf_file) + ')')
         quit()
 
     # get our current dconf custom keybinding config for comparison
@@ -116,28 +129,30 @@ if __name__ == '__main__':
         try:
             dconf_key = parseDconf(dconf_file)
         except configparser.MissingSectionHeaderError:
-            print('ERROR: %s is missing a Section Header' % dconf_file)
+            log.error('%s is missing a Section Header' % dconf_file)
         except configparser.ParsingError:
-            print('ERROR: %s has an incorrectly formatted (or missing) element in section %s' % (dconf_file,key_name))
+            log.error('%s has an incorrectly formatted (or missing) element in section %s' % (dconf_file,key_name))
+            log.error('%s has an incorrectly formatted (or missing) element in section %s' % (dconf_file,key_name))
         # todo check if the key-combination is in use with some other keybinding
         # we can handle having multiple keys defined per file
         process_file = True
         for key_name in dconf_key:
             if key_name != 'DEFAULT':
                 if keyNameExists(key_name, curr_conf) and key_name != '/':
-                    print('INFO: dconf already has key %s' % (key_name))
+                    log.debug('dconf already has key %s' % (key_name))
                     if not args.force:
                         process_file = False
                 elif key_name != '/':
                     # ugly, but if we don't check for trailing / it crashes our DE
                     if key_name.endswith('/'):
-                        key_ref.append(KEY_LOC + key_name)
+                        key_ref.append(DCONF_KEY_PATH + key_name)
                     else:
-                        key_ref.append(KEY_LOC + key_name + '/')
+                        key_ref.append(DCONF_KEY_PATH + key_name + '/')
+
         if process_file:
             out_conf.read(dconf_file)
         else:
-            print('INFO: NOT processing %s, keys already exist in dconf' % (dconf_file))
+            log.debug('NOT processing %s, keys already exist in dconf' % (dconf_file))
 
     # assemble our custom-keybindings array option, pop it into a tempfile for dconf load
     if len(key_ref) > 0:
@@ -153,12 +168,20 @@ if __name__ == '__main__':
         except configparser.DuplicateSectionError:
             pass
         out_conf.set('/', 'custom-keybindings', str(key_ref))
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as configfile:
-            out_conf.write(configfile)
-        # money shot
-        loadDconf(configfile.name)
+        # we must apply the root '/' section which includes sys overrides before applying the keys
+        out_root_conf = configparser.ConfigParser(interpolation=None)
+        out_root_conf.read_dict(out_conf)
+        for s in out_root_conf.sections():
+            if s != '/':
+                out_root_conf.remove_section(s)
+        # we don't need '/' section in our out_conf object now
+        out_conf.remove_section('/')
+        for conf in [ out_root_conf, out_conf]:
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as configfile:
+                conf.write(configfile)
+            # money shot
+            loadDconf(configfile.name)
         if not args.dry_run:
-            print('SUCCESS: config loaded, you can confirm with:')
-            print('  dconf dump %s' % KEY_LOC)
+            log.debug('Success! config loaded, you can confirm with:\n\tdconf dump %s' % DCONF_KEY_PATH)
     else:
-        print('WARNING: nothing to do!')
+        log.info('nothing to do!')
