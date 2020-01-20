1792bc23f8321be6c4297ef3ea8609a18305701e
David Mohammed <fossfreedom@ubuntu.com>
2020-01-20 14:13:33 +0000

    Remove all shuffler custom keybindings

--- etc/profile.d/10-budgie-desktop.sh
+++ etc/profile.d/10-budgie-desktop.sh
@@ -28,13 +28,13 @@ then
     touch ~/.config/budgie-desktop/changekeycontrol3
 fi
 
-if [ ! -f ~/.config/budgie-desktop/keycontrol5 ]
+if [ ! -f ~/.config/budgie-desktop/keycontrol6 ]
 then
     cd /usr/share/budgie-desktop/keycontrol/; . ./gnome-custom-keybinding-setup; cd
     mkdir -p ~/.config/budgie-desktop
     # delete old keycontrol files (if they have been previously created)
     rm -f ~/.config/budgie-desktop/keycontrol*
-    touch ~/.config/budgie-desktop/keycontrol5
+    touch ~/.config/budgie-desktop/keycontrol6
 fi
 
 if [ -f ~/.config/budgie-desktop/firstrun ]
